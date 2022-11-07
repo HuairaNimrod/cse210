4d2b7cbf266ce8315992a9a2060cc1473c563f1b
nimax <nimax29@gmail.com>
2022-11-07 01:43:35 -0700

    solo 1

---
+++ solo/solo1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Solo1
+{
+    public class Program
+    {
+        public static void Main(string[] args){
+
+            Console.Write("Whats your first name? ");
+            string firstname= Console.ReadLine();
+            Console.Write("Whats your last name? ");
+            string lastname= Console.ReadLine();
+            Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
+        }
+    }
+}
