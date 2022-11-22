fb767eae2706a07d37378b8fc5a5471e09d6dcc7
nimax <nimax29@gmail.com>
2022-11-22 05:00:18 -0700

    inheritance

---
+++ inheritance/Person.cs
@@ -0,0 +1,18 @@
+class Person
+{
+    private string _number;
+
+    public Person(string number)
+    {
+        _number = number;
+    }
+
+    public string GetNumber()
+    {
+        return _number;
+    }
+    public string GetName()
+    {
+        return "Nimrod";
+    }
+}
---
+++ inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program{
+
+        static void Main()
+        {
+            Student student = new Student("123123");
+            string name = student.GetName();
+            Console.WriteLine($"\n{name}\n");
+
+
+            
+
+            Estudiante estudiante = new Estudiante("01246813579");
+            string number = estudiante.GetNumber();
+            Console.WriteLine($"{number}\n");
+           
+        }
+
+        public class Student : Person
+        {
+          public Student(string number): base(number)
+          {
+
+          }
+        }
+
+        public class Estudiante : Person
+        {
+            public Estudiante(string number): base(number)
+            {
+
+            }
+        }
+    }
+}
