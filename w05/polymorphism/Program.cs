72615e84ba1c39c7d20fbfee1340837d13ac31cb
nimax <nimax29@gmail.com>
2022-11-28 23:27:46 -0700

    w05

---
+++ w05/polymorphism/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Polymorphism
+{
+    class Polymorphism
+    {
+
+        static void Main()
+        {
+            // create different kinds of employees...
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee());
+            employees.Add(new HourlyEmployee());
+
+            // get a custom calculation for each one
+            foreach(Employee employee in employees)
+            {
+                float pay = employee.CalculatePay();
+                Console.WriteLine(pay);
+            }
+        }       
+    }
+
+
+} 
